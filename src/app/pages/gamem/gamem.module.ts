7e724dab858eafe8b13dd67b76b2e5a8336154f0
Rafael <rafaelairesdemedeiros@outlook.com>
2024-02-14 23:24:15 -0300

    dialog confirmacao

--- src/app/pages/gamem/gamem.module.ts
+++ src/app/pages/gamem/gamem.module.ts
@@ -6,9 +6,19 @@ import { RippleModule } from 'primeng/ripple';
 import { GamemRoutingModule } from './gamem-routing.module';
 import { GamemComponent } from './gamem.component';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { ConfirmDialogCustom } from 'src/app/global/confirm-dialog';
 
 @NgModule({
   declarations: [GamemComponent, NavDesktopComponent],
-  imports: [CommonModule, GamemRoutingModule, ButtonModule, RippleModule],
+  imports: [
+    CommonModule,
+    GamemRoutingModule,
+    ButtonModule,
+    RippleModule,
+    ConfirmDialogModule,
+  ],
+  providers: [ConfirmationService, ConfirmDialogCustom],
 })
 export class GamemModule {}
