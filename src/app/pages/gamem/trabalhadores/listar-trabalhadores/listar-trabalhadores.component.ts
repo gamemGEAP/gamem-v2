64ed9d1a9e16a8c94f0ce1cb6eb0392f8ce22e66
Adauto Moisés <adautomaleandro@gmail.com>
2024-03-16 20:39:51 -0300

    Tela de listagem de trabalhadores

--- src/app/pages/gamem/trabalhadores/listar-trabalhadores/listar-trabalhadores.component.ts
+++ src/app/pages/gamem/trabalhadores/listar-trabalhadores/listar-trabalhadores.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TableCustomComponent } from 'src/app/global/components/table-custom/table-custom.component';
+import { ConfirmDialogCustom } from 'src/app/global/confirm-dialog';
+import { ToastCustom } from 'src/app/global/toast-custom';
+import { PageTemplate } from 'src/app/interfaces/page-template';
+import { Worker } from 'src/app/interfaces/worker';
+import { TrabalhadorService } from 'src/app/services/trabalhador.service';
 
 @Component({
   selector: 'app-listar-trabalhadores',
   templateUrl: './listar-trabalhadores.component.html',
-  styleUrls: ['./listar-trabalhadores.component.scss']
+  styleUrls: ['./listar-trabalhadores.component.scss'],
 })
-export class ListarTrabalhadoresComponent {
+export class ListarTrabalhadoresComponent implements OnInit {
+  listaTrabalhadores!: PageTemplate<Worker>;
+  trabalhador!: Worker;
+  paginaAtual: number = 0;
+  total = 0;
 
+  @ViewChild('tabela') tabela!: TableCustomComponent;
+
+  constructor(
+    private trabalhadorService: TrabalhadorService,
+    private confirmDialog: ConfirmDialogCustom,
+    private toast: ToastCustom
+  ) {}
+
+  ngOnInit(): void {
+    this.pageChange(0);
+  }
+
+  pageChange(pagina: number) {
+    this.trabalhadorService.list(pagina).subscribe((m) => {
+      const obj: any = m;
+      obj.content.map((trabalhador: any) => {
+        trabalhador.function = trabalhador.function
+          .map((f: any) => {
+            return f.name;
+          })
+          .reduce(
+            (accumulator: any, currentValue: any) =>
+              accumulator + ' ' + currentValue
+          );
+        return trabalhador;
+      });
+      this.tabela.setElementos(obj);
+      this.paginaAtual = pagina;
+    });
+  }
+
+  excluirTrabalhador(trabalhador: Worker) {
+    this.confirmDialog
+      .abrirDialog({
+        mensagem: `${trabalhador.name} será excluído(a) do sistema.`,
+        titulo: 'Tem certeza que deseja excluir?',
+      })
+      .subscribe((resp) => {
+        if (resp) {
+          this.trabalhadorService.delete(trabalhador.id).subscribe((m) => {
+            this.toast.sucess(m.message);
+            this.pageChange(this.paginaAtual);
+          });
+        }
+      });
+  }
 }
--- src/app/services/trabalhador.service.ts
+++ src/app/services/trabalhador.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
+import { PageTemplate } from '../interfaces/page-template';
+import { Worker } from '../interfaces/worker';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +15,15 @@ export class TrabalhadorService {
 
   public atendentes(): Observable<Array<string>> {
     return this.httpClient.get<Array<string>>(`${this.url}/name-attendants`);
+  }
+
+  public list(pagina: number): Observable<PageTemplate<Worker>> {
+    return this.httpClient.get<PageTemplate<Worker>>(
+      `${this.url}/?page=${pagina}&size=10`
+    );
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.httpClient.delete<any>(`${this.url}/delete-worker/?id=${id}`);
   }
 }
